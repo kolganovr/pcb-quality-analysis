90a863522ac07edb6914ba6785d1edda96d8335f
Roman Kolganov <kolganov2003@gmail.com>
2024-05-30 13:35:39 +0300

    new export

--- PZ4.py
+++ PZ4.py
@@ -17,7 +17,9 @@ if not os.path.exists('data'):
 # НА каждой строчке в диагональном итд должен быть словарь: Мат.ожидание, СКО, Точность, Крит.Ошибка
 
 # Создаем датафрейм для сохранения данных
-df = pd.DataFrame(columns=['Код файла', 'Диагональное', 'Горизонтальное', 'Вертикальное'])
+df = pd.DataFrame(columns=['Код', 'Диагональное_Мат.Ожид', 'Диагональное_СКО', 'Диагональное_Точность', 'Диагональное_Крит.Ошибка',
+                           'Горизонтальное_Мат.Ожид', 'Горизонтальное_СКО', 'Горизонтальное_Точность', 'Горизонтальное_Крит.Ошибка',
+                           'Вертикальное_Мат.Ожид', 'Вертикальное_СКО', 'Вертикальное_Точность', 'Вертикальное_Крит.Ошибка'])
 
 # Обрабатываем каждое изображение
 for i in tqdm(range(0, len(IMAGES))):
@@ -28,7 +30,7 @@ for i in tqdm(range(0, len(IMAGES))):
     # TODO: Динамический BLACKTRSHOLD в зависимости от ширины дорожек
     # FIXME: 045_ang и ang1 плохо считаются
 
-    treshold = 160
+    treshold = 120
 
     roads = np.array([])
     for row in img:
@@ -47,6 +49,12 @@ for i in tqdm(range(0, len(IMAGES))):
         roads = np.append(roads, r)
 
     roads = roads[roads > 5]
+    mean = np.mean(roads)
+    std = np.std(roads)
+
+    # remove all valuees that greater then mean + 3 std or less then mean - 3 std
+    roads = roads[roads < mean + 3 * std]
+    roads = roads[roads > mean - 3 * std]
 
     # Очищаем Plot
     plt.clf()
@@ -71,23 +79,34 @@ for i in tqdm(range(0, len(IMAGES))):
         raise Exception('Неизвестный угол')
     
     # Переводим ширины дорожек из пикселей (кол-во dpi - код файла) в мм
-    dpi = int(code)
+    dpi = 1200
     roads = roads / (dpi * 25.4) * 1000
     
     mean = np.mean(roads)
     std = np.std(roads)
     accuracy = np.count_nonzero(roads == roads[0]) / len(roads)
     error = np.count_nonzero(roads != roads[0]) / len(roads)
-    data = {'Мат.ожидание': mean, 'СКО': std, 'Точность': accuracy, 'Крит. ошибка': error}
+
+    data = [mean, std, accuracy, error]
 
     # Округляем все данные до 3 знака после запятой
-    data = {k: round(v, 3) for k, v in data.items()}
+    data = [np.round(x, 3) for x in data]
 
-    if code in df['Код файла'].values:
-        # Convert existing data to dictionary
-        df.loc[df['Код файла'] == code, angleTable] = df.loc[df['Код файла'] == code, angleTable].apply(lambda x: {**x, **data} if isinstance(x, dict) else data)
+    # Записываем данные в датафрейм
+    # Если этот код уже есть в датафрейме, а угол другой, то добавляем в соотвествующие столбцы
+
+    if code in df['Код'].values:
+        df.loc[df['Код'] == code, angleTable + '_Мат.Ожид'] = data[0]
+        df.loc[df['Код'] == code, angleTable + '_СКО'] = data[1]
+        df.loc[df['Код'] == code, angleTable + '_Точность'] = data[2]
+        df.loc[df['Код'] == code, angleTable + '_Крит.Ошибка'] = data[3]
     else:
-        df = df._append({'Код файла': code, angleTable: data}, ignore_index=True)
+        df = df._append({'Код': code,
+                        angleTable + '_Мат.Ожид': data[0],
+                        angleTable + '_СКО': data[1],
+                        angleTable + '_Точность': data[2],
+                        angleTable + '_Крит.Ошибка': data[3]}, ignore_index=True)
+        
     
 
 df.to_csv('data.csv', index=False)
